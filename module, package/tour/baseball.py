49c20f50961dbebdc5cd3a19c6514f9352a9f00c
jimin <w2037@e-mirim.hs.kr>
2021-09-10 15:41:40 +0900

    야구게임엔진 파이썬스럽게 완성

--- module, package/tour/baseball.py
+++ module, package/tour/baseball.py
@@ -1,45 +1,15 @@
-from random import random
+import baseball_game_engine
 
+# 반복
+while True:
+    # 숫자를 묻고
+    guess = input('답은 무엇일까여? ')
+    # strike, ball 편집하기
+    strike, ball = baseball_game_engine.check(guess, baseball_game_engine.answer)
+    # 출력하기
+    print(f'{guess}\strike:{strike}, ball : {ball}')
 
-def make_answer(self):
-    list_r = random.sample(range(9 + 1), 3)
-    return "".join(map(str, list_r))
-
-
-def check(guess, answer):
-    strike = 0
-    ball = 0
-    # 숫자 하나 꺼내서 정답에 있고, 자리가 같으면, strike  += 1
-    # 숫자 하나 꺼내서 정답에 있고, 자리가 다르면, ball += 1
-
-    for i, g in enumerate(guess):
-        for j, a in enumerate(answer):  # 숫자가 같으면
-            if g == a:
-                if i == j:
-                    strike += 1
-                else:
-                    ball += 1
-
-
-# guess[0] == answer[0]
-# guess[0] == answer[1]
-# guess[0] == answer[2]
-# guess[1] == answer[0]
-# guess[1] == answer[1]
-# guess[1] == answer[2]
-# guess[2] == answer[0]
-# guess[2] == answer[1]
-# guess[2] == answer[2]
-
-
-    return strike, ball
-
-if __name__ != '__main__':
-    answer = make_answer()
-    print(answer)
-    strike, ball = check("832", "934")
-    print(strike, ball)  # 1 0
-    strike, ball = check("431", "934")
-    print(strike, ball)  # 1 1
-    strike, ball = check("934", "934")
-    print(strike, ball)  # 3 0
+    # 정답 == 숫자 끝내기
+    if baseball_game_engine.answer == guess:
+        print("정답")
+        break
