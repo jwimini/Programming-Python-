442d77f2ad6cf3137998620cd66c15f594f0297a
jimin <w2037@e-mirim.hs.kr>
2021-10-01 15:37:08 +0900

    save text, with

--- file handler/save_text.py
+++ file handler/save_text.py
@@ -1,8 +1,15 @@
 f = open('text.txt', 'w', encoding='utf-8')
 
-f.write('wlalsd : ')
+f.write('지민:')
 f.write('연노랑')
 f.write('\n')   # 문단 내려가기
+f.write('지민:')
 f.write('핑크색')
 
-f.close()
+
+with open('text.txt','w', encoding='utf-8') as f:   # close가 필요없음 자동으로 닫아줌
+    f.write('지민:')
+    f.write('연노랑')
+    f.write('\n')  # 문단 내려가기
+    f.write('지민:')
+    f.write('핑크색')
