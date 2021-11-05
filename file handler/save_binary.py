e043bf99fe64f9989d18e1c0efedd765583bfe40
jimin <w2037@e-mirim.hs.kr>
2021-11-05 16:05:54 +0900

    pass

--- file handler/save_binary.py
+++ file handler/save_binary.py
@@ -1,3 +1,3 @@
-with open('binary.bin', 'rb') as f:
-    data = f.read()     #b'\xff\x00\x7f': 111111110000000001111111
-print(data)
+with open('binary.bin', 'wb') as f:
+    byte_list = bytes([255, 0,  127])
+    f.write(byte_list)
